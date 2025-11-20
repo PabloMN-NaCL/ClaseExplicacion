ca5c24bb5fc0dbd61e2ffa5bf75313654cb38ba5
PabloMNaCl <millannavarropablo@gmail.com>
2025-11-20 18:19:13 +0100

    Controlador de Auth. y Versioning

---
+++ Clase.Api.Identity/Controllers/AuthControler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Clase.Api.Identity.Controllers
+{
+    //Necesario para que los enpoint en Swagger Funiconen
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthControler : ControllerBase
+    {
+        private IEnumerable<User> _users = new List<User>();
+        
+        
+        //Endpoint para login
+        [HttpPost("register")]
+        //Interfaz 
+        public IActionResult RegisterUser([FromBody] User user)
+        {
+            _users.ToList().Add(user);
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var existingUser = _users.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+            if (existingUser != null)
+            {
+                return Ok("Login successful");
+            }
+            return Unauthorized("Invalid credentials");
+        }
+    }
+
+    public class User
+    {
+               public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+
--- Clase.Api.Identity/Program.cs
+++ Clase.Api.Identity/Program.cs
@@ -1,3 +1,5 @@
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+})
+.AddMvc() // This is needed for controllers
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
